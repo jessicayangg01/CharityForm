ec275cee6d5c79f61ae2e79df74f6ab7386d7ca8
Jessica Yang <59785485+jessicayangg01@users.noreply.github.com>
2023-10-26 15:02:38 -0400

    form UI
    the form for which users add their input

---
+++ CI_form.php
@@ -0,0 +1,12 @@
+
+    <form action=CI_insert.php method="post"/>
+    <p>Legal Name: <input type="text" name="LegalName"/></p>
+    <p>Common Name: <input type="text" name="CommonName"/></p>
+    <p>Street: <input type="text" name="Street"/></p>
+    <p>City: <input type="text" name="City"/></p>
+    <p>Province: <input type="text" name="Province"/></p>
+    <p>Postal Code: <input type="text" name="Postal_code"/></p>
+    <p>URL: <input type="text" name="myUrl"/></p>
+    <input type="submit" value="Submit"/>
+    
+
